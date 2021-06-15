d7f005b144f0a5260ddd089d51f02a654d590052
Steven Vroom <steven.klaas.vroom@gmail.com>
2021-06-15 12:50:47 +0200

    Could be

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import mongodb from "mongodb";
 const { MongoClient } = mongodb;
 
 const pass = process.env["DB_PASSWORD"];
-const uri = `mongodb+srv://ppplastic_app:${pass}@cluster0.4yp1e.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://ppplastic_app:${pass}@cluster0.4yp1e.mongodb.net/questionaire?retryWrites=true&w=majority`;
 
 export function getClient() {
     return new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
