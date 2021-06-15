74b2136840830cc50e53dae4f3f71b34e66897fa
Steven Vroom <steven.klaas.vroom@gmail.com>
2021-06-15 11:21:53 +0200

    Hide new password

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,8 @@
 import mongodb from "mongodb";
 const { MongoClient } = mongodb;
 
-const uri = "mongodb+srv://ppplastic_app:Cq3Dip5Jm9KYlVwI@cluster0.4yp1e.mongodb.net/questionaire?retryWrites=true&w=majority";
+const pass = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://ppplastic_app:${pass}@cluster0.4yp1e.mongodb.net/questionaire?retryWrites=true&w=majority`;
 
 export function getClient() {
     return new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
