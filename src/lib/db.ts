b5e058be12c38188d1910bb391463066dae1b07c
Steven Vroom <steven.klaas.vroom@gmail.com>
2021-06-15 13:06:43 +0200

    Stupid attempt

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import mongodb from "mongodb";
 const { MongoClient } = mongodb;
 
 const pass = process.env["DB_PASSWORD"];
-const uri = `mongodb+srv://ppplastic_app:7PPdd48OPnjUeQX5@cluster0.4yp1e.mongodb.net/questionaire?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://ppplastic_app:7PPdd48OPnjUeQX5@cluster0.4yp1e.mongodb.net/questionaire?retryWrites=true&w=majority";
 
 export function getClient() {
     return new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
