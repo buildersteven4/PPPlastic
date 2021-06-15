fa4a8d87b0991d036534314c6e824d90b1d3530a
Steven Vroom <steven.klaas.vroom@gmail.com>
2021-06-15 12:22:10 +0200

    I hate this

---
+++ src/routes/envhell.svelte
@@ -0,0 +1,13 @@
+<script>
+    let env = []
+    for (const key in process.env) {
+        if (Object.hasOwnProperty.call(process.env, key)) {
+            const element = process.env[key];
+            env.push([key, element])
+        }
+    }
+</script>
+
+{#each env as [k, v]}
+    {k}: {v}
+{/each}
