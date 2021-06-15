b263ee9de8aa025f5c88513625ef1902af086292
Steven Vroom <steven.klaas.vroom@gmail.com>
2021-06-15 12:03:39 +0200

    Add another test?

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,6 +5,6 @@ const pass = process.env.DB_PASSWORD;
 const uri = `mongodb+srv://ppplastic_app:${pass}@cluster0.4yp1e.mongodb.net/questionaire?retryWrites=true&w=majority`;
 
 export function getClient() {
-    throw JSON.stringify(process.env);
+    throw "WHAT? "+JSON.stringify(process.env);
     return new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 }
